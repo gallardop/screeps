2b8911ee9b6f151d52d9c40d733e343f21317a8b
Nick <nicolasverdicchio@gmail.com>
2020-01-31 00:48:39 -0300

    BASIC Centralizad Harvester Logic - pending to modularize outside main

--- role.harvester.js
+++ role.harvester.js
@@ -2,32 +2,36 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        if(creep.store.getFreeCapacity() > 0) {
-            var sources; 
-            if(!(typeof creep.memory.targetResource === 'undefined')) {
-                let targetResource = creep.memory.targetResource;
-                sources = [new RoomPosition(targetResource.x, targetResource.y, creep.room.name)];
-            } else {
-                sources = creep.room.find(FIND_SOURCES);
-                console.log(sources[0]);
-            }
-            
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
-                }
+        var hasCapacity = creep.store.getFreeCapacity() > 0;
+        var targetSource = creep.memory.targetSource;
+        var targetDefined = !(typeof targetSource === 'undefined');
+        var hasCollectedResources = creep.store.getUsedCapacity() > 0;
+        
+        if(hasCapacity && targetDefined) {
+            // Harvest
+            if(creep.harvest(targetSource) == ERR_NOT_IN_RANGE) {
+                // Not Close enough -> move to source
+                creep.moveTo(targetSource, {visualizePathStyle: {stroke: '#ffaa00'}});
+            } 
         }
         else {
-            var targets = creep.room.find(FIND_STRUCTURES, {
-                filter: (structure) => {
-                    return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
-                        structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-                }
-            });
-            if(targets.length > 0) {
-                if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
-                }
+            // Store collected resource
+            if(hasCollectedResources) {
+                // Find target structures to store collected resources
+                var targets = creep.room.find(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
+                    }
+                });
+                // Move & transfer resources
+                if(targets.length > 0) {
+                    if(creep.transfer(targets[0], RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                } 
             }
+
         }
     }
 };
